3d42655323b63d99fa3d187e53b82993666a0f2f
Sai Khawn Kham <khawnkham222@gmail.com>
2024-07-15 17:21:57 +0630

    fix unscrollable sidebar

--- src/state.js
+++ src/state.js
@@ -4,4 +4,7 @@ export const products = [
    { id: 3, name: "Cherry", price: 700 },
    { id: 4, name: "Date", price: 1000 },
    { id: 5, name: "Elderberry", price: 1400 },
+   { id: 6, name: "Orange", price: 500 },
+   { id: 7, name: "Mango", price: 1000 },
+   { id: 8, name: "Lemon", price: 2000 },
 ];
